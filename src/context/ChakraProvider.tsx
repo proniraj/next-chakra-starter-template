8c2e4add5a4dc75b67e6d82ea78adacd174d9628
ntnirajthakur21 <ntnirajthakur21@gmail.com>
2024-04-13 10:41:23 +0545

    Update ChakraProvider to include resetCSS prop in Chakra component

--- src/context/ChakraProvider.tsx
+++ src/context/ChakraProvider.tsx
@@ -5,15 +5,8 @@ import {
   ChakraProvider as Chakra,
   cookieStorageManager,
 } from "@chakra-ui/react";
-import dynamic from "next/dynamic";
 import React, { FC } from "react";
 import customTheme from "@/theme";
-
-// Lazy load the ColorModeStrictWrapper - this is a workaround for a bug in Chakra
-const ColorModeStrictWrapper = dynamic(
-  () => import("@/context/ColorModeStrictWrapper"),
-  { ssr: false },
-);
 
 type Props = {
   children: React.ReactNode;
@@ -22,8 +15,11 @@ type Props = {
 const ChakraProvider: FC<Props> = ({ children }) => {
   return (
     <CacheProvider>
-      <ColorModeStrictWrapper />
-      <Chakra colorModeManager={cookieStorageManager} theme={customTheme}>
+      <Chakra
+        colorModeManager={cookieStorageManager}
+        theme={customTheme}
+        resetCSS={true}
+      >
         {children}
       </Chakra>
     </CacheProvider>
